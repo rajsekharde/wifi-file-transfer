9c9fd8f0db09d3404a6131d35d293a907e2791c1
rajsekharde <rajsekharde612@gmail.com>
2026-02-15 20:18:01 +0530

    Add http server logging

--- handlers/listFiles.go
+++ handlers/listFiles.go
@@ -2,19 +2,33 @@ package handlers
 
 import (
 	// "fmt"
+	"encoding/json"
+	"github.com/fatih/color"
+	"log"
 	"net/http"
 	"os"
-	"encoding/json"
 )
 
 const uploadDir = "./uploads"
+
+var (
+	red   = color.RGB(255, 0, 0).SprintFunc()
+	green = color.RGB(0, 255, 0).SprintFunc()
+	blue  = color.RGB(200, 200, 255).SprintFunc()
+)
 
 func ListFilesHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Get all files
 	entries, err := os.ReadDir(uploadDir)
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		log.Printf("%s %s %s ERROR: %v",
+		blue(r.Method),
+		r.URL.Path,
+		red(http.StatusInternalServerError),
+		red(err),
+	)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -25,6 +39,14 @@ func ListFilesHandler(w http.ResponseWriter, r *http.Request) {
 			files = append(files, e.Name())
 		}
 	}
+
+	// Print log
+	log.Printf("%s %s %s %s",
+		blue(r.Method),
+		r.URL.Path,
+		green(http.StatusOK),
+		green("OK"),
+	)
 
 	// Set response header
 	w.Header().Set("Content-Type", "application/json")
--- main.go
+++ main.go
@@ -2,18 +2,37 @@ package main
 
 import (
 	"fmt"
+	"github.com/fatih/color"
+	"github.com/rajsekharde/wifi-file-transfer/handlers"
 	"log"
 	"net/http"
 	"strconv"
-	"github.com/rajsekharde/wifi-file-transfer/handlers"
 )
 
 const PORT = 8000
 
-func main() {
-	frontendHandler := http.FileServer(http.Dir("./static"))
+var (
+	// red   = color.RGB(255, 0, 0).SprintFunc()
+	green = color.RGB(0, 255, 0).SprintFunc()
+	blue  = color.RGB(200, 200, 255).SprintFunc()
+)
 
-	http.Handle("/", frontendHandler)
+func main() {
+
+	// Serve frontend on "/"
+	fs := http.FileServer(http.Dir("./static"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet && r.URL.Path == "/" {
+			// Print log
+			log.Printf("%s %s %s %s",
+				blue(r.Method),
+				r.URL.Path,
+				green(http.StatusOK),
+				green("OK"),
+			)
+		}
+		fs.ServeHTTP(w, r)
+	})
 
 	http.HandleFunc("/files", handlers.ListFilesHandler)
 
