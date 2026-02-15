fec8a67334cc04636e0a728f464ffe69577ce3bd
rajsekharde <rajsekharde612@gmail.com>
2026-02-15 18:08:21 +0530

    Run basic Go http server

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+const PORT = 8000
+
+func main() {
+	http.HandleFunc("/", func (w http.ResponseWriter, r* http.Request) {
+		fmt.Fprintln(w, "Hello World!")
+	})
+
+	fmt.Printf("Server running on port %d\n", PORT)
+	http.ListenAndServe(":"+strconv.Itoa(PORT), nil)
+}
