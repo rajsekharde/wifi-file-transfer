36e5b712f00d0ed0deccb6a6ccc1116befe1a80c
rajsekharde <rajsekharde612@gmail.com>
2026-02-17 00:54:56 +0530

    Fix UI

--- static/script.js
+++ static/script.js
@@ -5,12 +5,31 @@ async function loadFiles() {
     const files = await res.json();
 
     const fileList = document.getElementById("fileList");
-    fileList.innerHTML = '';
 
     files.forEach( f => {
-        const li = document.createElement('li');
-        li.innerHTML = `${f} <a href="/download/${f}">Download</a>`;
-        fileList.appendChild(li);
+        const fileDiv = document.createElement('div');
+        const fileDetailsDiv = document.createElement('div');
+        const fileName = document.createElement('p');
+        const fileSize = document.createElement('p');
+        const downloadButton = document.createElement('button');
+
+        fileDiv.id = "fileDiv";
+        fileDetailsDiv.id = "fileDetailsDiv";
+        fileName.id = "fileName";
+        downloadButton.id = "downloadButton";
+
+        fileName.textContent = f;
+        fileSize.textContent = "10000B"
+
+        downloadButton.textContent = "Download";
+        downloadButton.onclick = (() => {
+            window.location.href = `/download/${f}`;
+        });
+        // fileDiv.innerHTML = `${f} <a href="/download/${f}">Download</a>`;
+        
+        fileDetailsDiv.append(fileName, fileSize);
+        fileDiv.append(fileDetailsDiv, downloadButton);
+        fileList.appendChild(fileDiv);
     })
 }
 
