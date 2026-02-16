33713413d38015633a6f65dde499dfd346cc20e7
rajsekharde <rajsekharde612@gmail.com>
2026-02-17 01:45:09 +0530

    Fix UX: disable buttons on upload and download

--- static/script.js
+++ static/script.js
@@ -7,29 +7,42 @@ async function loadFiles() {
     const fileList = document.getElementById("fileList");
     fileList.innerHTML = '';
 
-    files.forEach( f => {
+    files.forEach(f => {
         const fileDiv = document.createElement('div');
         const fileDetailsDiv = document.createElement('div');
         const fileName = document.createElement('p');
         const fileSize = document.createElement('p');
         const downloadButton = document.createElement('button');
 
-        fileDiv.id = "fileDiv";
-        fileDetailsDiv.id = "fileDetailsDiv";
-        fileName.id = "fileName";
-        fileSize.id = "fileSize";
-        downloadButton.id = "downloadButton";
+        fileDiv.classList.add("fileDiv");
+        fileDetailsDiv.classList.add("fileDetailsDiv");
+        fileName.classList.add("fileName");
+        fileSize.classList.add("fileSize");
+        downloadButton.classList.add("downloadButton");
 
         fileName.textContent = f.name;
         fileSize.textContent = `${f.size} B`
 
         downloadButton.textContent = "Download";
-        downloadButton.onclick = (() => {
-            window.location.href = `/download/${f.name}`;
-            document.getElementById("fileInput").value = "";
+
+        downloadButton.onclick = ((e) => {
+            downloadButton.textContent = "Downloading...";
+            downloadButton.disabled = true;
+
+            const link = document.createElement("a");
+            link.href = `/download/${f.name}`;
+            link.download = f.name;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+            setTimeout(() => {
+                downloadButton.textContent = "Download";
+                downloadButton.disabled = false;
+            }, 1000);
         });
         // fileDiv.innerHTML = `${f} <a href="/download/${f}">Download</a>`;
-        
+
         fileDetailsDiv.append(fileName, fileSize);
         fileDiv.append(fileDetailsDiv, downloadButton);
         fileList.appendChild(fileDiv);
@@ -41,19 +54,28 @@ loadFiles()
 
 // Upload files
 async function upload() {
+    const uploadButton = document.getElementById("uploadButton");
+    uploadButton.textContent = "Uploading";
+    uploadButton.disabled = true;
+
     const file = document.getElementById('fileInput').files[0];
 
-    if(!file)
-        return alert('Choose file')
-
+    if (!file) {
+        uploadButton.textContent = "Upload";
+        uploadButton.disabled = false;
+        return alert('Choose file');
+    }
     const formData = new FormData();
     formData.append('file', file);
 
-    await fetch ('/upload', {
+    await fetch('/upload', {
         method: 'POST',
         body: formData
     });
 
     document.getElementById("fileInput").value = "";
+    uploadButton.textContent = "Upload";
+    uploadButton.disabled = false;
+
     loadFiles();
 }
