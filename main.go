c62b534b4f9c32fab9967a900fdcd4c05298d8bf
rajsekharde <rajsekharde612@gmail.com>
2026-02-16 15:21:14 +0530

    Add global paths for running compiled binary

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+	"os"
+	"path/filepath"
 )
 
 const PORT = 8000
@@ -20,9 +22,22 @@ var (
 )
 
 func main() {
+	exePath, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	realPath, err := filepath.EvalSymlinks(exePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	baseDir := filepath.Dir(realPath)
+	staticDir := filepath.Join(baseDir, "static")
 
 	// Serve frontend on "/"
-	fs := http.FileServer(http.Dir("./static"))
+	// fs := http.FileServer(http.Dir("./static"))
+	fs := http.FileServer(http.Dir(staticDir))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		startTime := time.Now()
 		fs.ServeHTTP(w, r)
